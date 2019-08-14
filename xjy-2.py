c6813dfea94f9acb15b36a8222ec61a355baf6fb
010DaweiZhou <1034057622@qq.com>
2019-08-14 22:22:47 +0800

    the 3 session

--- xjy-2.py
+++ xjy-2.py
@@ -1,7 +1,18 @@
-temp = input("you guess number:")
-guess = int(temp)
-if guess == 8:
-    print("right!")
-else:
-    print("wrong!")
+import random
+
+value = random.randint(1,10)
+
+while(1):
+    temp = input("you guess number:")
+    guess = int(temp)
+    
+    if guess == value:
+        print("right!")
+        break
+    else:
+        if guess > value:
+            print("too big !")
+        if guess < value:
+            print("too small !")
+
 print("end , bye!")
---
+++ xjy-3.py
@@ -0,0 +1,67 @@
+teacher = "me"
+print(teacher)
+
+teacher = "you"
+print(teacher)
+
+x = 3
+x = 5
+y = 8
+z = x + y
+print(z)
+
+zhou = " hello"
+
+print(teacher + zhou)
+
+x = '5'
+y = '8'
+print(x+y)
+
+x = 'let\'s '
+y = 'do it!'
+print(x + y)
+
+string = 'C:\\now\\'
+print(string)
+
+string = """hello 
+
+world """
+print(string)
+
+a = 0.000000000000000000000000000000000000000001
+print(a)
+
+print(True + True)
+print(True + False)
+
+
+a = '520'
+print(int(a))
+print(float(a))
+print(str(a))
+
+a = 4.99
+print(int(a))
+
+a = 5e15
+print(a)
+
+print(type(a))
+print(type('520'))
+print(type(1))
+
+print(isinstance(1,int))
+
+print(3/2)
+print(3//2)
+
+a = b = c = d = 10
+print(a,b,c,d)
+
+print(3**2)
+
+print(not True)
+
+print(3<4<5)
